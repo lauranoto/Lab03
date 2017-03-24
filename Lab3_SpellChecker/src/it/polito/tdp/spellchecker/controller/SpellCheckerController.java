9dd1a58e30f71a1c004f4332b7275cf1e6865c9d
Giulia <Giulia@Giulia-PC.station>
2017-03-24 12:10:21 +0100

    versione intermedia

--- Lab3_SpellChecker/src/it/polito/tdp/spellchecker/controller/SpellCheckerController.java
+++ Lab3_SpellChecker/src/it/polito/tdp/spellchecker/controller/SpellCheckerController.java
@@ -1,5 +1,62 @@
 package it.polito.tdp.spellchecker.controller;
 
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextArea;
+
 public class SpellCheckerController {
-	
+
+    @FXML
+    private ResourceBundle resources;
+
+    @FXML
+    private URL location;
+
+    @FXML
+    private ComboBox<String> cmbLanguage;
+
+    @FXML
+    private TextArea txtTesto;
+
+    @FXML
+    private Button btnSpellCheck;
+
+    @FXML
+    private TextArea txtResult;
+
+    @FXML
+    private Label txtErrors;
+
+    @FXML
+    private Button btnClearText;
+
+    @FXML
+    private Label txtTime;
+
+    @FXML
+    void doClearText(ActionEvent event) {
+
+    }
+
+    @FXML
+    void doSpellCheck(ActionEvent event) {
+
+    }
+
+    @FXML
+    void initialize() {
+        assert cmbLanguage != null : "fx:id=\"cmbLanguage\" was not injected: check your FXML file 'SpellChecker.fxml'.";
+        assert txtTesto != null : "fx:id=\"txtTesto\" was not injected: check your FXML file 'SpellChecker.fxml'.";
+        assert btnSpellCheck != null : "fx:id=\"btnSpellCheck\" was not injected: check your FXML file 'SpellChecker.fxml'.";
+        assert txtResult != null : "fx:id=\"txtResult\" was not injected: check your FXML file 'SpellChecker.fxml'.";
+        assert txtErrors != null : "fx:id=\"txtErrors\" was not injected: check your FXML file 'SpellChecker.fxml'.";
+        assert btnClearText != null : "fx:id=\"btnClearText\" was not injected: check your FXML file 'SpellChecker.fxml'.";
+        assert txtTime != null : "fx:id=\"txtTime\" was not injected: check your FXML file 'SpellChecker.fxml'.";
+
+    }
 }
---
+++ Lab3_SpellChecker/src/it/polito/tdp/spellchecker/model/Dictionary.java
@@ -0,0 +1,17 @@
+package it.polito.tdp.spellchecker.model;
+
+import java.util.List;
+
+public class Dictionary {
+	
+	public void loadDictionary(String language){
+		
+	}
+	
+	public List<RichWord> spellCheckText(List<String> inputTextList){
+		return null;
+	}
+
+
+
+}
